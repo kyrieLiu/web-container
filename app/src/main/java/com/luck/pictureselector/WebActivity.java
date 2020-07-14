db98d7cc7ab5950165d74d53ada4dd3d25eeebd5
15153246027 <652992429@qq.com>
2020-07-14 18:38:20 +0800

    做外壳

---
+++ app/src/main/java/com/luck/pictureselector/WebActivity.java
@@ -0,0 +1,366 @@
+package com.luck.pictureselector;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.CookieSyncManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.animators.AnimationType;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+import com.luck.picture.lib.listener.OnResultCallbackListener;
+import com.luck.pictureselector.widget.SelectMenuDialog;
+
+import java.io.File;
+import java.util.List;
+
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+/**
+ * Created by liuyin on 2019/4/15 14:28
+ * Describe: 加载Web页面
+ */
+public class WebActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private WebView webView;
+
+    private String webUrl = "https://192.168.199.100:3001/examination";
+    //private String webUrl = "http://192.168.199.100:8848/htmlDemo/file.html";
+
+    private ValueCallback valueCallback;
+
+    private int RC_CAMERA_AND_LOCATION = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        requestPermission();
+        initView();
+    }
+
+    private void initView() {
+        progressBar = findViewById(R.id.pb_webview_load);
+        webView = findViewById(R.id.wv_web);
+        initWebView();
+        webView.loadUrl(webUrl);
+        CookieSyncManager.createInstance(this);
+        CookieSyncManager.getInstance().sync();
+    }
+
+
+    public void initWebView() {
+
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setAllowFileAccess(true);
+        webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSetting.setSupportZoom(false);
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setUseWideViewPort(true);
+        webSetting.setSupportMultipleWindows(false);
+
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setJavaScriptEnabled(true);
+        // 当页面宽度大于WebView宽度时，缩小使页面宽度等于WebView宽度
+        webSetting.setLoadWithOverviewMode(true);
+        webSetting.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSetting.setGeolocationEnabled(true);
+
+
+        webSetting.setAppCacheEnabled(true);
+        webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
+        webSetting.setAppCachePath(this.getDir("appcache", 0).getPath());
+        webSetting.setDatabasePath(this.getDir("databases", 0).getPath());
+        webSetting.setGeolocationDatabasePath(this.getDir("geolocation", 0)
+                .getPath());
+        webSetting.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
+
+
+        webView.addJavascriptInterface(new Object() {
+            @JavascriptInterface
+            public void h5_finish() {
+                WebActivity.this.finish();
+            }
+
+        }, "android");
+
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new MyWebChromeClient());
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    webView.getSettings()
+                            .setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+                }
+                handler.proceed();
+            }
+        });
+
+    }
+
+    // Web视图
+    private class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public void onReceivedSslError(WebView webView, SslErrorHandler sslErrorHandler, SslError sslError) {
+            sslErrorHandler.proceed();
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onLoadResource(WebView view, String url) {
+            super.onLoadResource(view, url);
+        }
+
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            //加载完成后可以调用这个查看HTML源码
+            //webView.loadUrl("javascript:android.processHTML(document.documentElement.outerHTML);");
+
+        }
+    }
+
+    protected class MyWebChromeClient extends WebChromeClient {
+
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+            valueCallback = filePathCallback;
+            if (fileChooserParams.isCaptureEnabled()) {
+                openCamera();
+            } else {
+                openGallery();
+            }
+            return true;
+        }
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            if (newProgress == 100) {
+                progressBar.setVisibility(GONE);
+            } else if (progressBar.getVisibility() == GONE) {
+                progressBar.setVisibility(VISIBLE);
+            }
+            progressBar.setProgress(newProgress);
+        }
+    }
+
+    private void requestPermission() {
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this, "申请权限",
+                    RC_CAMERA_AND_LOCATION, perms);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+
+    private void openGallery() {
+
+
+        // 进入相册 以下是例子：不需要的api可以不写
+        PictureSelector.create(this)
+                .openGallery(PictureMimeType.ofImage())// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+                .imageEngine(GlideEngine.createGlideEngine())// 外部传入图片加载引擎，必传项
+                .theme(R.style.picture_default_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style v2.3.3后 建议使用setPictureStyle()动态方式
+                .isWeChatStyle(false)// 是否开启微信图片选择风格
+                .isUseCustomCamera(false)// 是否使用自定义相机
+                .isPageStrategy(true)// 是否开启分页策略 & 每页多少条；默认开启
+                .setRecyclerAnimationMode(AnimationType.DEFAULT_ANIMATION)// 列表动画效果
+                .isWithVideoImage(true)// 图片和视频是否可以同选,只在ofAll模式下有效
+                .isMaxSelectEnabledMask(true)// 选择数到了最大阀值列表是否启用蒙层效果
+                .maxSelectNum(1)// 最大图片选择数量
+                .minSelectNum(1)// 最小选择数量
+                .maxVideoSelectNum(1) // 视频最大选择数量
+                //.minVideoSelectNum(1)// 视频最小选择数量
+                .imageSpanCount(4)// 每行显示个数
+                .isReturnEmpty(false)// 未选择数据时点击按钮是否可以返回
+                //.isAndroidQTransform(false)// 是否需要处理Android Q 拷贝至应用沙盒的操作，只针对compress(false); && .isEnableCrop(false);有效,默认处理
+                .setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)// 设置相册Activity方向，不设置默认使用系统
+                .isOriginalImageControl(false)// 是否显示原图控制按钮，如果设置为true则用户可以自由选择是否使用原图，压缩、裁剪功能将会失效
+                .selectionMode(PictureConfig.SINGLE)// 多选 or 单选
+                .isSingleDirectReturn(true)// 单选模式下是否直接返回，PictureConfig.SINGLE模式下有效
+                .isPreviewImage(true)// 是否可预览图片
+                .isCamera(true)// 是否显示拍照按钮
+                .isEnableCrop(false)// 是否裁剪
+                .synOrAsy(true)//同步true或异步false 压缩 默认同步
+                .freeStyleCropEnabled(false)// 裁剪框是否可拖拽
+                .circleDimmedLayer(false)// 是否圆形裁剪
+                .hideBottomControls(false)
+                .showCropFrame(false)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
+                .showCropGrid(false)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
+                .isPreviewEggs(false)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中)
+                .cutOutQuality(90)// 裁剪输出质量 默认100
+                .minimumCompressSize(100)// 小于100kb的图片不压缩
+                .forResult(new OnResultCallbackListener<LocalMedia>() {
+
+                    @Override
+                    public void onResult(List<LocalMedia> result) {
+                        String path = result.get(0).getPath();
+                        Log.d("tag", "path==" + path);
+                        Uri uri = Uri.parse(path);
+                        Uri[] uris = {uri};
+                        valueCallback.onReceiveValue(uris);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        valueCallback.onReceiveValue(null);
+                    }
+                });
+
+    }
+
+    private void openCamera() {
+
+
+        // 进入相册 以下是例子：不需要的api可以不写
+        PictureSelector.create(this)
+                .openCamera(PictureMimeType.ofImage())// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+                .imageEngine(GlideEngine.createGlideEngine())// 外部传入图片加载引擎，必传项
+                .theme(R.style.picture_default_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style v2.3.3后 建议使用setPictureStyle()动态方式
+                .isWeChatStyle(false)// 是否开启微信图片选择风格
+                .isUseCustomCamera(false)// 是否使用自定义相机
+                .isPageStrategy(true)// 是否开启分页策略 & 每页多少条；默认开启
+                .setRecyclerAnimationMode(AnimationType.DEFAULT_ANIMATION)// 列表动画效果
+                .isWithVideoImage(true)// 图片和视频是否可以同选,只在ofAll模式下有效
+                .isMaxSelectEnabledMask(true)// 选择数到了最大阀值列表是否启用蒙层效果
+                .maxSelectNum(1)// 最大图片选择数量
+                .minSelectNum(1)// 最小选择数量
+                .maxVideoSelectNum(1) // 视频最大选择数量
+                //.minVideoSelectNum(1)// 视频最小选择数量
+                .imageSpanCount(4)// 每行显示个数
+                .isReturnEmpty(false)// 未选择数据时点击按钮是否可以返回
+                //.isAndroidQTransform(false)// 是否需要处理Android Q 拷贝至应用沙盒的操作，只针对compress(false); && .isEnableCrop(false);有效,默认处理
+                .setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)// 设置相册Activity方向，不设置默认使用系统
+                .isOriginalImageControl(false)// 是否显示原图控制按钮，如果设置为true则用户可以自由选择是否使用原图，压缩、裁剪功能将会失效
+                .selectionMode(PictureConfig.SINGLE)// 多选 or 单选
+                .isSingleDirectReturn(true)// 单选模式下是否直接返回，PictureConfig.SINGLE模式下有效
+                .isPreviewImage(true)// 是否可预览图片
+                .isCamera(true)// 是否显示拍照按钮
+                .isEnableCrop(false)// 是否裁剪
+                .synOrAsy(true)//同步true或异步false 压缩 默认同步
+                .freeStyleCropEnabled(false)// 裁剪框是否可拖拽
+                .circleDimmedLayer(false)// 是否圆形裁剪
+                .hideBottomControls(false)
+                .showCropFrame(false)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
+                .showCropGrid(false)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
+                .isPreviewEggs(false)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中)
+                .cutOutQuality(90)// 裁剪输出质量 默认100
+                .minimumCompressSize(100)// 小于100kb的图片不压缩
+                .forResult(new OnResultCallbackListener<LocalMedia>() {
+
+                    @Override
+                    public void onResult(List<LocalMedia> result) {
+                        String path = result.get(0).getPath();
+                        Log.d("tag", "path==" + path);
+                        Uri uri = Uri.parse(path);
+                        Uri[] uris = {uri};
+                        valueCallback.onReceiveValue(uris);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        valueCallback.onReceiveValue(null);
+                    }
+                });
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            String url=webView.getUrl();
+            String []urlArr=url.split("/");
+            String lastPath=urlArr[urlArr.length-1];
+            if ("paper".equals(lastPath)){
+                showWarningDialog();
+            }else{
+                webView.goBack();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void showWarningDialog(){
+        SelectMenuDialog dialog=new SelectMenuDialog(this);
+        dialog.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (webView != null) {
+            webView.stopLoading();
+            //
+            webView.clearCache(true);
+//            webView.clearHistory();
+//            webView.pauseTimers();
+            webView.destroy();
+            webView = null;
+
+        }
+    }
+}
